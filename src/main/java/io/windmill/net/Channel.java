e3365953744160561e61866c52c623e7220663cc
Jordan West <jordanrw@gmail.com>
2016-02-16 18:46:05 -0800

    add documentation

--- src/main/java/io/windmill/net/Channel.java
+++ src/main/java/io/windmill/net/Channel.java
@@ -1,6 +1,7 @@
 package io.windmill.net;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
@@ -10,10 +11,19 @@ import io.windmill.core.Future;
 import io.windmill.core.Status;
 import io.windmill.core.Status.Flag;
 import io.windmill.core.tasks.Task1;
+import io.windmill.core.tasks.VoidTask1;
 import io.windmill.net.io.InputStream;
 import io.windmill.net.io.OutputStream;
 import io.windmill.utils.IOUtils;
 
+/**
+ * Windmill's equivalent of {@link java.nio.channels.SocketChannel},
+ * providing an interface to stream-oriented connecting sockets.
+ * Each {@link Channel} is assigned to a specific {@link CPU} and all work
+ * on the socket or its streams. Channel's can be obtained using
+ * {@link CPU#connect(InetSocketAddress)} or
+ * {@link CPU#listen(InetSocketAddress, VoidTask1, VoidTask1)}
+ */
 public class Channel implements AutoCloseable
 {
     private final CPU cpu;
@@ -34,16 +44,28 @@ public class Channel implements AutoCloseable
         this.output = new OutputStream(cpu, key, channel);
     }
 
+    /**
+     * Perform a task continuously on the {@link CPU} this {@link Channel} is assigned to.
+     * Similar to {@link CPU#repeat(Task1)}
+     * @param task the work to perform
+     * @param <O> the type of value the work produces
+     */
     public <O> void loop(Task1<CPU, Future<O>> task)
     {
         cpu.repeat((cpu) -> task.compute(cpu).map((v) -> Status.of(Flag.CONTINUE)));
     }
 
+    /**
+     * @return the {@link InputStream} that can be used to read data sent to this {@link Channel}
+     */
     public InputStream getInput()
     {
         return input;
     }
 
+    /**
+     * @return the {@link InputStream} that can be used to write data to this {@link Channel}
+     */
     public OutputStream getOutput()
     {
         return output;
